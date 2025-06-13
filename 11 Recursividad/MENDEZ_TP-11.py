dee8b7b3f1703e617ae826b02db38402616e8032
Roberto Mendez <robermen02@gmail.com>
2025-06-13 17:30:26 -0300

    TP Recursividad

--- 11 Recursividad/MENDEZ_TP-11.py
+++ 11 Recursividad/MENDEZ_TP-11.py
@@ -34,7 +34,27 @@ if __name__ == "__main__":
 # exponente, utilizando la fórmula 𝑛𝑚 = 𝑛∗𝑛(𝑚−1). Prueba esta función en un 
 # algoritmo general. 
 
+def potencia_recursiva(n, m):
+    if m <= 0:
+        return 1
+    else:
+        return n * potencia_recursiva(n, m-1)
 
+
+if __name__ == "__main__":
+    print(potencia_recursiva(2, 3))
 
 # 4) Crear una función recursiva en Python que reciba un número entero positivo en base 
 # decimal y devuelva su representación en binario como una cadena de texto.
+
+import math as m
+
+def dec_a_bin(decimal):
+    if decimal == 0:
+        return 0
+    else:
+        res = decimal%2
+        return str(dec_a_bin(m.floor(decimal/2))) + str(res)
+        
+if __name__ == "__main__":
+    print(dec_a_bin(78))

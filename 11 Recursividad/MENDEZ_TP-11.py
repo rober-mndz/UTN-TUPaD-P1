11245361eefeb1f36dcacfe6369ae3fa5b64d36f
rober_mndz <robermen02@gmail.com>
2025-06-13 14:46:07 -0300

    Avance TP Recursividad

---
+++ 11 Recursividad/MENDEZ_TP-11.py
@@ -0,0 +1,20 @@
+# 1) Crea una función recursiva que calcule el factorial de un número. Luego, utiliza esa 
+# función para calcular y mostrar en pantalla el factorial de todos los números enteros 
+# entre 1 y el número que indique el usuario 
+
+
+
+# 2) Crea una función recursiva que calcule el valor de la serie de Fibonacci en la posición 
+# indicada. Posteriormente, muestra la serie completa hasta la posición que el usuario 
+# especifique. 
+
+
+
+# 3) Crea una función recursiva que calcule la potencia de un número base elevado a un 
+# exponente, utilizando la fórmula 𝑛𝑚 = 𝑛∗𝑛(𝑚−1). Prueba esta función en un 
+# algoritmo general. 
+
+
+
+# 4) Crear una función recursiva en Python que reciba un número entero positivo en base 
+# decimal y devuelva su representación en binario como una cadena de texto.

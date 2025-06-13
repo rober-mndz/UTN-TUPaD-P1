22f18a5d78e0750d3d4e33c8f1350a444bfe5b88
rober_mndz <robermen02@gmail.com>
2025-06-13 14:59:15 -0300

    Recursividad avance

--- 11 Recursividad/MENDEZ_TP-11.py
+++ 11 Recursividad/MENDEZ_TP-11.py
@@ -2,13 +2,33 @@
 # función para calcular y mostrar en pantalla el factorial de todos los números enteros 
 # entre 1 y el número que indique el usuario 
 
-
+def factorial(num):
+    if num == 0 or num == 1:
+        return 1
+    else:
+        return num * factorial(num - 1)
+    
+if __name__ == "__main__":
+    num = int(input("Ingrese un numero: "))
+    for i in range(1, num + 1):
+        print(factorial(i))
 
 # 2) Crea una función recursiva que calcule el valor de la serie de Fibonacci en la posición 
 # indicada. Posteriormente, muestra la serie completa hasta la posición que el usuario 
 # especifique. 
 
+def fibonacci(pos):
+    if pos <= 0:
+        return 1
+    elif pos == 1:
+        return 0
+    else:
+        return fibonacci(pos-1) + fibonacci(pos-2)
 
+    
+if __name__ == "__main__":
+    num = int(input("Ingresa la posicion:"))
+    print(fibonacci(num))
 
 # 3) Crea una función recursiva que calcule la potencia de un número base elevado a un 
 # exponente, utilizando la fórmula 𝑛𝑚 = 𝑛∗𝑛(𝑚−1). Prueba esta función en un 
